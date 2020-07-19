0720198cbdcf46874f607b0108e210f195fba58a
Donlon <iDonlon@hotmail.com>
2020-07-19 18:54:48 +0800

    Update pv.js

--- js/mw/pv.js
+++ js/mw/pv.js
@@ -1,6 +1,43 @@
 !(function ($, mw) {
-  $(function () {
-    mw.loader.load('https://xh.localhost.mco.moe/mw/pv.js');
-    console.log('Hello, ' + mediaWiki.config.get('wgPageName'))
-  })
+	function getViewCount(data) {
+		let sum = 0;
+		for (day in data) {
+			sum += data[day]
+		}
+		return sum
+	}
+
+	function showMonthlyPV(data) {
+		let pvBtn = $('<span>').attr('class', 'mw-custom-monthly-pv').text('PV/mo: ' + getViewCount(data).toLocaleString())
+
+		pvBtn.css('font-size', 'small')
+		    .css('font-weight', 'normal')
+		    .css('margin-left', '0.6em')
+		    .css('vertical-align', 'baseline')
+		    .css('line-height', '1em')
+		    .css('font-family', 'sans-serif')
+		$('#firstHeading').append(pvBtn)
+	}
+
+	if (mw.config.get('wgIsArticle') && mw.config.get('wgAction') === "view") {
+		let articleId = mw.config.get('wgArticleId') + '';
+	    $.ajax({
+	        type: 'GET',
+	        dataType: 'json',
+	        url: mw.util.wikiScript('api'),
+	        data: {
+	            'action': 'query',
+	            'pageids': articleId,
+	            'prop': 'pageviews',
+	            'format': 'json'
+	        },
+	        success: function (data) {
+	        	let pv = data.query.pages[articleId].pageviews
+	        	showMonthlyPV(pv)
+	        },
+	        error: function (e) {
+	        	console(e)
+	        }
+		})
+	}
 }) (jQuery, mediaWiki)
