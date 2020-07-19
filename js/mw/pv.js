89228ab7a1dd7462233b8bc66beff7c5c1c70c5d
Donlon <iDonlon@hotmail.com>
2020-07-19 18:06:33 +0800

    Update pv.js

--- js/mw/pv.js
+++ js/mw/pv.js
@@ -1,3 +1,6 @@
-$(function () {
-  alert('Hello, ' + mediaWiki.config.get('wgPageName'))
-})
+!(function ($, mw) {
+  $(function () {
+    mw.loader.load('https://xh.localhost.mco.moe/mw/pv.js');
+    console.log('Hello, ' + mediaWiki.config.get('wgPageName'))
+  })
+}) (jQuery, mediaWiki)
